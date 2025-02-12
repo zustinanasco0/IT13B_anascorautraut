8a6f330513e2b171cc81c52b1fc1c13c180bef78
Zustin Mervin Paul A. Añasco <zustinanasco0@gmail.com>
2025-02-12 09:35:47 +0800

    Update arithmetics_operators.java

--- anascorautraut_IT13B/src/prelim/arithmetics_operators.java
+++ anascorautraut_IT13B/src/prelim/arithmetics_operators.java
@@ -1,36 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package prelim;
-
-import java.util.Scanner;
+public class IT13B_anasco {
+    
+}
+package IT13B_Anasco;
 
 /**
  *
  * @author User
  */
-public class arithmetics_operators {
-  
-    public static void main(String[] args) {
-       
-         int x;
-         int y;
+public class Lab_act1_Aritmetics{
+    public static void main(String[] args){
         
-       Scanner in = new Scanner(System.in);
-             
-       System.out.println(" Enter first number: ");
-       x = in.nextInt();
-        System.out.println(" Enter second number: ");
-       y = in.nextInt();
-       
-       
-            
-       System.out.println(x + " + " + y + " = " + (x+y));
-       System.out.println(x + " - " + y + " = " + (x+y));
-       System.out.println(x + " x " + y + " = " + (x+y));
-       System.out.println(x + " / " + y + " = " + (x+y));
-       System.out.println(x + " mod " + y + " = " + (x+y));
+        int first = 10;
+        int second = 4;
+        int third = 6;
+        int fourth = 10 + 4 + 6;
+        
+        int fourthadd = first + second + third;
+        int fourthsub = first - second - third;
+        int fourthmul = first * second * third;
+        int fourthdiv = first / second / third;
+        int fourthmod = first % second % third;
+        
+        
+        System.out.println("10 + 4 + 6 =" + fourthadd);
+        System.out.println("10 - 4 + 6 =" + fourthsub);
+        System.out.println("10 * 4 + 6 =" + fourthmul);
+        System.out.println("10 / 4 + 6 =" + fourthdiv);
+       System.out.println("10 % 4 + 6 =" + fourthmod);
+        
+    }
+    
+    
+    
 }
-}
+
+
+  
 
